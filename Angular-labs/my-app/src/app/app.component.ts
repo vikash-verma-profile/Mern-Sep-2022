b313729ea3311652bcdbf9b5d273403ed529c5d8
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-30 14:05:00 +0530

    added code

--- Angular-labs/my-app/src/app/app.component.ts
+++ Angular-labs/my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Customer } from './app.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  imgUrl="././assets/sample.jpg";
+  CustomerModel:Customer=new Customer();
+  CustomerModels:Array<Customer>=new Array<Customer>();
+  ClickMe(){
+  this.CustomerModels.push(this.CustomerModel);
+  console.log(this.CustomerModels);
+  this.CustomerModel=new Customer();
+  }
 }
