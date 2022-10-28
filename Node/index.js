b36cf23176e499f6c68865d4fdca90430acb14cc
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-10-28 13:23:44 +0530

    added insertion code

--- Node/index.js
+++ Node/index.js
@@ -1,5 +1,21 @@
 var http=require('http');
 
+var MongoClient=require('mongodb').MongoClient;
+var url="mongodb://localhost:27017/"
+MongoClient.connect(url,function(err,db){
+if(err) throw err;
+var dbo=db.db("customerDB");
+var customerObj={
+    customerCode: "101",
+    customerName: "Vikash Verma",
+    customerAmount: 100
+};
+dbo.collection("customers").insertOne(customerObj,function(err,res){
+
+    if(err) throw err;
+    db.close();
+});
+});
 http.createServer(function(req,res){
 res.end('Vikash Verma');
 }).listen(8081);
