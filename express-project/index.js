16ba49fbec6f320f63d5af3b3c5bb5bb112660db
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-12-02 12:35:38 +0530

    sample project on express

--- express-project/index.js
+++ express-project/index.js
@@ -1,8 +1,19 @@
 const express=require('express');
 const app=express();
 
-app.get('/',(req,res)=>{
-    res.send("Helllo");
+app.set('view engine','jade')
+
+app.get('/',function(req,res){
+res.render('index');
+});
+app.get('/home',(req,res)=>{
+    res.send("Helllo, This is home router");
+});
+app.get('/login',(req,res)=>{
+    res.send("Helllo, This is login router");
+});
+app.get('/profile',(req,res)=>{
+    res.send("Helllo, This is profile router");
 });
 
 app.listen(process.env.port || 3000);
